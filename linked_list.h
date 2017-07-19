ffb3ccd1606203579c033f03083fb6dcf6527465
xiacijie <cijie@ualberta.ca>
2017-07-20 00:06:48 +0800

    Update linked_list.h

--- linked_list.h
+++ linked_list.h
@@ -2,15 +2,20 @@
 // Created by Jack Xia on 2017-07-19.
 //
 
+//----------------directives-----------------------------
 #ifndef C_PROGRAMMING_LINKED_LIST_H
 #define C_PROGRAMMING_LINKED_LIST_H
 
 #define PUBLIC
 #define PRIVATE static
 
+//-----------------declarations--------------------------
 typedef int ITEM;
-PRIVATE void terminate(char *message);
+PRIVATE void terminate(const char *message);
 PUBLIC struct linked_list *create();
 PUBLIC void add(struct linked_list *list,ITEM i);
-PUBLIC void print_int(struct linked_list *list);
+PUBLIC void print_int(const struct linked_list *list);
+PUBLIC ITEM pop(struct linked_list *list);
+PUBLIC int get_size(const struct linked_list *list);
+PUBLIC int search_index(const struct linked_list *list,ITEM to_search);
 #endif //C_PROGRAMMING_LINKED_LIST_H
